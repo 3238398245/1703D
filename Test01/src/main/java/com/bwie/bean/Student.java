0945d6b7b25fcd4ad92c60706c35ae987bd89dc2
admin <2455805320@qq.com>
2019-07-10 18:36:00 +0800

    上传项目

---
+++ Test01/src/main/java/com/bwie/bean/Student.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package com.bwie.bean;
+
+/**
+ * @姓名：董天宇
+ *
+ * @时间：2019年7月7日
+ */
+public class Student {
+	public static void main(String[] args) {
+		
+		System.out.println(1);
+		System.out.println(1);
+		System.out.println(1);
+		
+	}
+}
---
+++ Test01/src/test/java/com/bwie/test/Test01.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+package com.bwie.test;
+
+/**
+ * @姓名：董天宇
+ *
+ * @时间：2019年7月7日
+ */
+public class Test01 {
+	
+	public void show(){
+		
+		System.out.println("添加信息");
+	}
+	
+}
